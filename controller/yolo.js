65bb49e57e6a216d7ce2dd6e65d06eff17905d73
carlzhao1996 <carl1996@outlook.com>
2019-12-08 21:01:32 +0800

    updated on imageProcess function

--- controller/yolo.js
+++ controller/yolo.js
@@ -4,49 +4,57 @@ const shell = require('shelljs');
 const path = require('path');
 const fs = require('fs');
 //Result image path
-const imgDir = path.resolve(__dirname,'../')+'/ResultImages';
+const imgDir = path.resolve(__dirname, '../') + '/ResultImages';
 fs.existsSync(imgDir) || fs.mkdirSync(imgDir);
 //Image storage path
-const imgStorageDir = path.resolve(__dirname,'../')+'/ImageStorage';
+const imgStorageDir = path.resolve(__dirname, '../') + '/ImageStorage';
 fs.existsSync(imgStorageDir) || fs.mkdirSync(imgStorageDir);
 
-function shellExec(command)
-{
-    return new Promise((resolve,reject)=>{
-        shell.exec(command,(code,output)=>{
-            return resolve({code,output});
+function shellExec(command) {
+    return new Promise((resolve, reject) => {
+        shell.exec(command, (code, output) => {
+            return resolve({ code, output });
         });
-    }).catch((err)=>{
+    }).catch((err) => {
         console.error(err)
         return err
     });
 }
 //POST: yolo/imageProcess
-router.post('/imageProcess',async(req,res)=>{
+router.post('/imageProcess', async (req, res) => {
     try {
         //TODO: use cache to storage image name?
         const imgName = req.body.imgName;
-        if(!imgName){
+        if (!imgName) {
             return res.json("image Name is required");
         }
-        const command = './yoloExec.sh '+imgName;
-        let result = await shellExec(command);
+        const command = './yoloExec.sh ' + imgName;
+        let data = await shellExec(command);
+        let output = data.output;
+        let result = {}
+        arr = output.split("\n");
+        let resultOutput = []
+        for (let i = 5; i < arr.length - 3; i++) {
+            resultOutput.push(arr[i]);
+        }
+        result.code = data.code;
+        result.output = resultOutput;
         return res.json(result);
     } catch (error) {
         return res.json(error);
     }
-    
+
 });
 //POST: yolo/imageSave
-router.post('/imageSave',async(req,res)=>{
+router.post('/imageSave', async (req, res) => {
     const file = req.files.image;
     console.log(file);
     try {
-        fs.writeFile(imgDir+'/result.png',file.data,(err)=>{
-            if(err){
+        fs.writeFile(imgDir + '/result.png', file.data, (err) => {
+            if (err) {
                 console.error(err);
                 return res.json(err)
-            }else{
+            } else {
                 console.log('success');
                 return res.json('success')
             }
@@ -58,12 +66,12 @@ router.post('/imageSave',async(req,res)=>{
 });
 
 //POST: yolo/imageUpload
-router.post('/imageUpload',async(req,res)=>{
+router.post('/imageUpload', async (req, res) => {
     const file = req.files.file.data;
     const fileName = req.files.file.name;
     try {
-        fs.writeFileSync(imgStorageDir+'/'+fileName,file,()=>{});
-        const commad = 'cp imageStorage/'+fileName+' '+'/Users/wenhaozhao/Desktop/FYP_Demo/yolov3';
+        fs.writeFileSync(imgStorageDir + '/' + fileName, file, () => { });
+        const commad = 'cp imageStorage/' + fileName + ' ' + '/Users/wenhaozhao/Desktop/FYP_Demo/yolov3';
         let result = await shellExec(commad);
         //TODO: memory cache to storage image file name
         return res.json(result);
